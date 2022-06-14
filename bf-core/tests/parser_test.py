c4638f65165abde6a5428154c7aa2839b6068736
Victor Ndaba <mwangindaba@gmail.com>
2022-06-14 03:26:56 +0300

    🧪 test(bf-core): added global tests for all packages

---
+++ bf-core/tests/parser_test.py
@@ -0,0 +1,64 @@
+import unittest
+
+from parsing.parser import Parser
+
+class TestMain(unittest.TestCase):
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+        self.p = Parser()
+
+    def test_sections(self):
+        ast = self.p.parse("""
+        section Interfaces {}
+        """
+        )
+
+        expected_ast = {
+            "type": "Specfile",
+            "sections": [
+                {
+                    "type": "SECTION",
+                    "name": "Interfaces",
+                    "body": []
+                }
+            ]
+        }
+
+        self.assertEqual(ast, expected_ast)
+
+    def test_variables(self):
+        ast = self.p.parse(""" 
+        section Interfaces {
+            interface Rest {
+                isVersioned = true;
+            }
+        }   
+        """)
+        
+        expected = {
+            "type": "Specfile",
+            "sections": [
+                {
+                    "type": "SECTION",
+                    "name": "Interfaces",
+                    "body": [
+                        {
+                            "type": "INTERFACE",
+                            "name": "Rest",
+                            "body": [
+                                {
+                                "type": "ASSIGNMENT",
+                                "name": "isVersioned",
+                                "value": "true"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+
+        self.assertEqual(ast, expected)
+
+if __name__ == "__main__":
+    unittest.main()
