45490a00526508a921726c04b139067c6ee47989
Victor Ndaba <mwangindaba@gmail.com>
2022-07-14 05:43:36 +0300

    📦 feat: updated serialization for fn calls

--- bf-core/serde/serialize.py
+++ bf-core/serde/serialize.py
@@ -25,31 +25,41 @@ def write_block(s, depth: int):
     for item in s['body']:
         if item['type'] == "ASSIGNMENT":
             name = item['name']
-            value: str = item['value']
 
-            if value.__contains__("|"):
-                # tis array
-                v = value.replace("|", ",")
-                temp = f"{name} = [{v},];"
+            if isinstance(item['value'], dict):
+                value: dict = item['value']
+                fn_name = value['function']
+                fn_arg = value['args']
+                temp = f"{name} = {fn_name}({fn_arg});"
+
                 template += f"\t{indent}{temp}\n"
-            elif value.__contains__(";"):
-                # tis an object
-                template += f"\t{indent}{name} = "
-                template += "{\n"
-
-                fields = value.split(";")
-                for f in fields:
-                    pair = f.split("->")
-                    k = pair[0]
-                    v = pair[1]
-
-                    template += f"\t{indent}\t{k}: {v},\n"
-                
-                template += f"\t{indent}"
-                template += "};\n"
             else:
-                temp = f"{name} = {value};"
-                template += f"\t{indent}{temp}\n"
+
+                value: str = item['value']
+
+                if value.__contains__("|"):
+                    # tis array
+                    v = value.replace("|", ",")
+                    temp = f"{name} = [{v},];"
+                    template += f"\t{indent}{temp}\n"
+                elif value.__contains__(";"):
+                    # tis an object
+                    template += f"\t{indent}{name} = "
+                    template += "{\n"
+
+                    fields = value.split(";")
+                    for f in fields:
+                        pair = f.split("->")
+                        k = pair[0]
+                        v = pair[1]
+
+                        template += f"\t{indent}\t{k}: {v},\n"
+                    
+                    template += f"\t{indent}"
+                    template += "};\n"
+                else:
+                    temp = f"{name} = {value};"
+                    template += f"\t{indent}{temp}\n"
 
         else:
             block = write_block(item , depth+1)
